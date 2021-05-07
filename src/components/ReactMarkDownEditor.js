62e1ea862fc061e38d51324aad186a3edbf5215c
yoosh199 <64148999+yoosh199@users.noreply.github.com>
2021-05-07 14:27:40 +0900

    create ReactMarkDownEditor

---
+++ src/components/ReactMarkDownEditor.js
@@ -0,0 +1,19 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import MDEditor from "@uiw/react-md-editor";
+// No import is required in the WebPack.
+// import "@uiw/react-md-editor/dist/markdown-editor.css";
+
+const mkdStr = "";
+
+function ReactMarkDownEditor(props) {
+  console.log(props.text);
+  const [value, setValue] = React.useState(props.text);
+  return (
+    <div className="container">
+      <MDEditor.Markdown source={value} />
+    </div>
+  );
+}
+
+export default ReactMarkDownEditor;
