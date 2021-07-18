0200be7a8f14194f4755fff3bd446ddb2490ff16
Si-Hyeon Yoo <64148999+yoosh199@users.noreply.github.com>
2021-07-18 17:27:02 +0900

    add: Rightbar

---
+++ src/components/Rightbar.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./Rightbar.css";
+
+
+const Rightbar = () => {
+
+    
+
+    return(
+        <div className="rightbar">
+            <div className="rightbar_index">
+            목차  <i class="fas fa-bars"></i>
+            </div>
+            
+        </div>
+    );
+}
+
+export default Rightbar;
