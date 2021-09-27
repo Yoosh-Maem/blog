0852d5f0c9d95616a604c0c8a52593b5a73257ea
Si-Hyeon Yoo <64148999+yoosh199@users.noreply.github.com>
2021-09-27 22:22:01 +0900

    Update MainHeader.js

--- src/components/MainHeader.js
+++ src/components/MainHeader.js
@@ -1,17 +1,18 @@
 import React from "react";
 
 import "./MainHeader.css";
+import { Link } from "react-router-dom";
 
 const MainHeader = () => {
+  return (
+    <header>
+      <Link to="/" className="header_blog_icon">
+        아이콘자리
+      </Link>
 
-    return(
-        <header>
-            <div className="header_blog_icon">아이콘자리
-            </div>
-            <div className="header_main_title">Yoosh & MaeM</div>
-        </header>
-    );
-
-}
+      <div className="header_main_title">Yoosh & MaeM</div>
+    </header>
+  );
+};
 
 export default MainHeader;
